7644a896fbdfeb1a831122a69a7e392f9d0aed01
Gabriel-Buttenbender <gabriel-buttenbender@hotmail.com>
2014-10-21 21:24:52 -0200

    Update Sistemas.cpp

--- Sistemas.cpp
+++ Sistemas.cpp
@@ -1,1 +1,5 @@
-Arquivo reservado para anotacoes de sistemas futuros!
+Arquivo reservado para anotacoes de sistemas futuros e feitos!
+
+Registro / Login: 
+  * Concluido: Salvamento de: Level, Dinheiro, Exp, Senha;
+  * Falta: Adicionar Cores nas Box's e mensagens;
